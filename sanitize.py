c8349b9355ebf407df8b13f218b5b6a69c91f0b5
Cliff Hults <cliff.hults@gmail.com>
2025-07-03 15:34:57 -0400

    Add multiple file input

--- sanitize.py
+++ sanitize.py
@@ -5,8 +5,9 @@ import pandas as pd
 parser = argparse.ArgumentParser(
     description="Sanitize CSV file by removing specific columns."
 )
-parser.add_argument("input_file", type=str, help="Path to the input CSV file.")
-
+parser.add_argument(
+    "filenames", type=str, help="Path to the input CSV file.", nargs=argparse.REMAINDER
+)
 args = parser.parse_args()
 
 filtered_cols = [
@@ -14,5 +15,9 @@ filtered_cols = [
     "Donor Name",
     "Donor Comment",
 ]
-f = pd.read_csv(args.input_file, usecols=lambda x: x not in filtered_cols)
-f.to_csv(args.input_file, index=False)
+
+dono_files = args.filenames
+print(f"Processing Files: {dono_files}")
+for file in dono_files:
+    f = pd.read_csv(file, usecols=lambda x: x not in filtered_cols)
+    f.to_csv(file, index=False)
